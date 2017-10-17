826c66f3e63050aa8818ee56f3765b448671744b
Christina Hildebrandt <32872335+christinahil@users.noreply.github.com>
2017-10-17 15:11:06 +0200

    Update demo.m

--- src/demo.m
+++ src/demo.m
@@ -1,1 +1,11 @@
+add_path_to_suitesparse();
 
+A = load('case14.mat');
+ 
+poolobj = parpool(2);
+
+[Z,lu_factors] = compute_coefficients(A);
+
+[V] = calculate_thevenin(A,Z,lu_factors);
+
+delete(poolobj);
